0d4d0289b0a8f59af09903251ad54a9575f10f1a
ravishansenevirathna <travishan55@gmail.com>
2024-11-24 20:19:19 +0530

    6th commit

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,5 +11,6 @@ namespace EcommerceApi.Models
         public string Name{get; set;}
         public decimal Price{get; set;}
         public int Qty{get; set;}
+        public string Category{get; set;}
     }
 }
--- obj/Debug/net8.0/EcommerceApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5abe16d4b0f09abf11579fa24146e084920818")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5572641d6e342d99b1e02b03bdd36663bfc1c90c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
